f93b5f89451bb40d04154e45a5ec8c58f45ba20e
ClaudeF4491 <93078203+ClaudeF4491@users.noreply.github.com>
2022-04-27 22:02:52 -0400

    Update alpha_homora_pools_scrape.sql

--- schemas/alpha_homora_pools_scrape.sql
+++ schemas/alpha_homora_pools_scrape.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS alpha_homora_pools_scrape (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     timestamp TIMESTAMP,
     chain text,
     strategy text,
@@ -18,6 +18,7 @@ CREATE TABLE IF NOT EXISTS alpha_homora_pools_scrape (
     tvl_pool float NULL,
     tvl_homora float NULL,
     positions int NULL,
+    INDEX idx_timestamp (timestamp),	
     INDEX idx_pool (pool),
     INDEX idx_chain (chain),
     INDEX idx_protocol (protocol),
