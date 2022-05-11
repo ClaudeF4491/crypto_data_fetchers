6ebb39e3904603b824553024a7e6dbc9029d7555
ClaudeF4491 <93078203+ClaudeF4491@users.noreply.github.com>
2022-05-10 20:31:52 -0400

    simplify cream data importer to raw only (#10)

--- scripts/import_history_to_database.py
+++ scripts/import_history_to_database.py
@@ -12,11 +12,11 @@ python3 scripts/import_history_to_database.py \
     test_table \
     3306
 """
-from adapters.database_adapter import CreamFinanceState, DatabaseAdapter
+from adapters.database_adapter import DatabaseAdapter
 from datetime import datetime
 import json
 import logging
-import numpy as np
+import pandas as pd
 from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 import typer
@@ -37,6 +37,7 @@ def create_datamodel(tablename: str) -> Base:
         timestamp = Column(DateTime)
         event = Column(String)
         metadata_ = Column("metadata", String)
+        source = Column(String)
 
     return DataRecord
 
@@ -83,125 +84,26 @@ def insert(
     # Intialize a session
     session = db.create_session()
     metadata = {"label": label, "type": "history"}
+    source = "cream"
 
-    # Save off state objects
-    states: List[CreamFinanceState] = list()
-    logging.info("Creating state objects ...")
-    for r in results:
-        decimals = int(r["underlyingDecimals"])
-        # Create a structured event
-        cream_state = CreamFinanceState(
-            timestamp=datetime.fromisoformat(r["date"].split("Z")[0]),
-            address=r["address"],
-            comptroller=r["comptroller"],
-            symbol=r["symbol"],
-            underlying_symbol=r["underlying_symbol"],
-            underlyingDecimals=decimals,
-        )
-
-        # Everything else is nullable, cast if applicable
-        # TODO: Wow, this is ugly. Hardcoded copy-pasta. Make generic somehow
-        t = r.get("borrow_apy")
-        if t:
-            cream_state.borrow_apy = float(t)
-            if np.isnan(cream_state.borrow_apy):
-                cream_state.borrow_apy = None
-
-        t = r.get("supply_apy")
-        if t:
-            cream_state.supply_apy = float(t)
-            if np.isnan(cream_state.supply_apy):
-                cream_state.supply_apy = None
-
-        t = r.get("utilization_rate")
-        if t:
-            cream_state.utilization_rate = float(t)
-            if np.isnan(cream_state.utilization_rate):
-                cream_state.utilization_rate = None
-
-        t = r.get("cash")
-        if t:
-            cream_state.cash = float(int(t) / 10**decimals)
-            if np.isnan(cream_state.cash):
-                cream_state.cash = None
-
-        t = r.get("cashUSD")
-        if t:
-            cream_state.cashUSD = float(t)
-            if np.isnan(cream_state.cashUSD):
-                cream_state.cashUSD = None
-
-        t = r.get("totalBorrows")
-        if t:
-            cream_state.totalBorrows = float(int(t) / 10**decimals)
-            if np.isnan(cream_state.totalBorrows):
-                cream_state.totalBorrows = None
-
-        t = r.get("totalBorrowsUSD")
-        if t:
-            cream_state.totalBorrowsUSD = float(t)
-            if np.isnan(cream_state.totalBorrowsUSD):
-                cream_state.totalBorrowsUSD = None
-
-        t = r.get("totalReserves")
-        if t:
-            cream_state.totalReserves = float(int(t) / 10**decimals)
-            if np.isnan(cream_state.totalReserves):
-                cream_state.totalReserves = None
-
-        t = r.get("totalReservesUSD")
-        if t:
-            cream_state.totalReservesUSD = float(t)
-            if np.isnan(cream_state.totalReservesUSD):
-                cream_state.totalReservesUSD = None
-
-        t = r.get("borrowRatePerBlock")
-        if t:
-            cream_state.borrowRatePerBlock = float(int(t) / 10**decimals)
-            if np.isnan(cream_state.borrowRatePerBlock):
-                cream_state.borrowRatePerBlock = None
-
-        t = r.get("supplyRatePerBlock")
-        if t:
-            cream_state.supplyRatePerBlock = float(int(t) / 10**decimals)
-            if np.isnan(cream_state.supplyRatePerBlock):
-                cream_state.supplyRatePerBlock = None
-
-        t = r.get("exchangeRate")
-        if t:
-            cream_state.exchangeRate = float(int(t) / 10**decimals)
-            if np.isnan(cream_state.exchangeRate):
-                cream_state.exchangeRate = None
-
-        # Attampt to infer price using one of a few different field combos
-        if cream_state.cash and cream_state.cashUSD:
-            cream_state.price = cream_state.cashUSD / cream_state.cash
-        if cream_state.totalReserves and cream_state.totalReservesUSD:
-            cream_state.price = cream_state.totalReservesUSD / cream_state.totalReserves
-        if cream_state.totalBorrows and cream_state.totalBorrowsUSD:
-            cream_state.price = cream_state.totalBorrowsUSD / cream_state.totalBorrows
-
-        states.append(cream_state)
-
-    # Save states
-    session.bulk_save_objects(states)
-    logging.info(f"Committing {len(states)} state records to database ...")
-    session.commit()
-    logging.info(f"Committed {len(states)} records.")
-
-    # Save off raw events
+    # Save off raw events, grouping-by-date into list of lists
+    df = pd.DataFrame(results)
+    inds_by_date = df.groupby("date").indices
     logging.info("Creating raw event objects ...")
     events: List[DataRecord] = list()
-    for r in results:
+    for dt, inds in inds_by_date.items():
         # Create a raw event
+        group_events = [results[i] for i in inds]
         event = DataRecord(
-            timestamp=datetime.fromisoformat(r["date"].split("Z")[0]),
-            event=json.dumps(r),
+            timestamp=datetime.fromisoformat(dt.split("Z")[0]),
+            event=json.dumps([group_events]),
             metadata_=json.dumps(metadata),
+            source=source,
         )
         events.append(event)
 
     # Save raw events
+    logging.info(f"Bulk saving {len(events)} event records ...")
     session.bulk_save_objects(events)
     logging.info(f"Committing {len(events)} raw event records to database ...")
     session.commit()
